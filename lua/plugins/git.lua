234351b3842f6759cf26764f3bfa795fdb72af07
pgijsbertsen <117165507+pgijsbertsen@users.noreply.github.com>
2024-08-18 12:06:28 +0200

    Add git-blame

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,16 +1,26 @@
 return {
-	"kdheepak/lazygit.nvim",
-	cmd = {
-		"LazyGit",
-		"LazyGitConfig",
-		"LazyGitCurrentFile",
-		"LazyGitFilter",
-		"LazyGitFilterCurrentFile",
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
 	},
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	keys = {
-		{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+	{
+		"f-person/git-blame.nvim",
+		event = "VeryLazy",
+		opts = {
+			enabled = true,
+			date_format = "%Y-%m-%d %H:%M",
+		},
 	},
 }
