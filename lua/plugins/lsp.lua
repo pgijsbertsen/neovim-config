2b287acb1769f27136244900b3d4656922043549
pgijsbertsen <117165507+pgijsbertsen@users.noreply.github.com>
2024-08-25 21:16:40 +0200

    Use LSP-zero

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,63 +1,98 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		{ "williamboman/mason.nvim", config = true },
-		"williamboman/mason-lspconfig.nvim",
-	},
-	config = function()
-		require("mason").setup({
-			ui = {
-				border = "rounded",
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+	{
+		{
+			"VonHeikemen/lsp-zero.nvim",
+			branch = "v4.x",
+			lazy = true,
+			config = false,
+		},
+		{
+			"williamboman/mason.nvim",
+			lazy = false,
+			config = true,
+		},
 
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"ansiblels", -- Ansible
-				"bashls", -- Bash
-				"bzl", -- Starlark
-				"docker_compose_language_service", -- Docker Compose
-				"gopls", -- GO
-				"jinja_lsp", -- Jinja
-				"lua_ls", -- Lua
-				"pyright", -- Python
-				"terraformls", -- Terraform
-				"yamlls", -- YAML
+		-- Autocompletion
+		{
+			"hrsh7th/nvim-cmp",
+			event = "InsertEnter",
+			dependencies = {
+				{ "L3MON4D3/LuaSnip" },
 			},
-			handlers = {
-				function(server_name)
-					require("lspconfig")[server_name].setup({})
-				end,
-			},
-		})
+			config = function()
+				local cmp = require("cmp")
 
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
-			callback = function(event)
-				local map = function(keys, func, desc)
-					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+				cmp.setup({
+					sources = {
+						{ name = "nvim_lsp" },
+					},
+					mapping = cmp.mapping.preset.insert({
+						["<C-Space>"] = cmp.mapping.complete(),
+						["<C-u>"] = cmp.mapping.scroll_docs(-4),
+						["<C-d>"] = cmp.mapping.scroll_docs(4),
+					}),
+					snippet = {
+						expand = function(args)
+							vim.snippet.expand(args.body)
+						end,
+					},
+				})
+			end,
+		},
+
+		-- LSP
+		{
+			"neovim/nvim-lspconfig",
+			cmd = { "LspInfo", "LspInstall", "LspStart" },
+			event = { "BufReadPre", "BufNewFile" },
+			dependencies = {
+				{ "hrsh7th/cmp-nvim-lsp" },
+				{ "williamboman/mason.nvim" },
+				{ "williamboman/mason-lspconfig.nvim" },
+			},
+			config = function()
+				local lsp_zero = require("lsp-zero")
+
+				local lsp_attach = function(event)
+					local map = function(keys, func, desc)
+						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
+
+					map("<leader>gd", require("telescope.builtin").lsp_definitions, "Goto Definition")
+					map("<leader>gr", require("telescope.builtin").lsp_references, "Goto References")
+					map("<leader>gi", require("telescope.builtin").lsp_implementations, "Goto Implementation")
+					map("<leader>go", require("telescope.builtin").lsp_type_definitions, "Type Definition")
+					map("<leader>p", require("telescope.builtin").lsp_document_symbols, "Document Symbols")
+					map("<leader>P", require("telescope.builtin").lsp_workspace_symbols, "Workspace Symbols")
+					map("<leader>Ps", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Workspace Symbols")
 				end
 
-				map("gd", require("telescope.builtin").lsp_definitions, "Goto Definition")
-				map("gr", require("telescope.builtin").lsp_references, "Goto References")
-				map("gi", require("telescope.builtin").lsp_implementations, "Goto Implementation")
-				map("go", require("telescope.builtin").lsp_type_definitions, "Type Definition")
-				map("<leader>p", require("telescope.builtin").lsp_document_symbols, "Document Symbols")
-				map("<leader>P", require("telescope.builtin").lsp_workspace_symbols, "Workspace Symbols")
-				map("<leader>Ps", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Workspace Symbols")
+				lsp_zero.extend_lspconfig({
+					sign_text = true,
+					lsp_attach = lsp_attach,
+					capabilities = require("cmp_nvim_lsp").default_capabilities(),
+				})
 
-				map("gl", vim.diagnostic.open_float, "Open Diagnostic Float")
-				map("K", vim.lsp.buf.hover, "Hover Documentation")
-				map("gs", vim.lsp.buf.signature_help, "Signature Documentation")
-				map("gD", vim.lsp.buf.declaration, "Goto Declaration")
-				map("ga", vim.lsp.buf.code_action, "Code Action")
+				require("mason-lspconfig").setup({
+					ensure_installed = {
+						"ansiblels", -- Ansible
+						"bashls", -- Bash
+						"bzl", -- Starlark
+						"docker_compose_language_service", -- Docker Compose
+						"gopls", -- GO
+						"jinja_lsp", -- Jinja
+						"lua_ls", -- Lua
+						"pyright", -- Python
+						"terraformls", -- Terraform
+						"yamlls", -- YAML
+					},
+					handlers = {
+						function(server_name)
+							require("lspconfig")[server_name].setup({})
+						end,
+					},
+				})
 			end,
-		})
-	end,
+		},
+	},
 }
