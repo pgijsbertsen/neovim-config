293c0edde9b960da01aad5fc891804178a94776c
pgijsbertsen <117165507+pgijsbertsen@users.noreply.github.com>
2024-08-28 09:56:46 +0200

    Remove unused autocmd

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -108,23 +108,5 @@ return {
         end
       end,
     },
-    autocmds = {
-      neotree_refresh = {
-        event = "TermClose",
-        pattern = "*lazygit*",
-        desc = "Refresh Neo-Tree sources when closing lazygit",
-        callback = function()
-          local manager_avail, manager = pcall(require, "neo-tree.sources.manager")
-          if manager_avail then
-            for _, source in ipairs({ "filesystem", "git_status", "document_symbols" }) do
-              local module = "neo-tree.sources." .. source
-              if package.loaded[module] then
-                manager.refresh(require(module).name)
-              end
-            end
-          end
-        end,
-      },
-    },
   },
 }
