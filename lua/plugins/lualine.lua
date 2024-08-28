3872def6d8add113a9fc106a59e50bff18bb5322
pgijsbertsen <117165507+pgijsbertsen@users.noreply.github.com>
2024-08-28 10:24:04 +0200

    Indenting fixes

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,14 +1,14 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	opts = {
-		tabline = {
-			lualine_a = { "mode" },
-			lualine_b = { "branch", "diff" },
-			lualine_c = { "buffers" },
-			lualine_x = { "diagnostics", "filetype" },
-			lualine_y = { "progress" },
-			lualine_z = { "location" },
-		},
+  "nvim-lualine/lualine.nvim",
+  opts = {
+    tabline = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff" },
+      lualine_c = { "buffers" },
+      lualine_x = { "diagnostics", "filetype" },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
+    },
     sections = {},
-	},
+  },
 }
