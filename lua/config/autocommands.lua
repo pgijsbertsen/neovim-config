fb6d5f3dc576d27522b26728298fc0e2edd028e4
pgijsbertsen <117165507+pgijsbertsen@users.noreply.github.com>
2025-03-29 09:50:08 +0100

    Add Lazy update autocmd

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -1,3 +1,17 @@
+-- Autoupdate Lazy plugins on startup
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    group = augroup("autoupdate"),
+    callback = function()
+        if require("lazy.status").has_updates then
+            require("lazy").update({ show = false, })
+        end
+    end,
+})
+
 -- highlight when yanking text
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
